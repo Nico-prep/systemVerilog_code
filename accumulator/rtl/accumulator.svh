c4b46f21114e02459d6dff966fedfebb7a207515
Nico-prep <nicologuarducci@gmail.com>
2026-08-02 14:23:57 +0200

    added overload detector for positive numbers

--- accumulator/rtl/accumulator.svh
+++ accumulator/rtl/accumulator.svh
@@ -4,24 +4,33 @@ module accumulator(
     input logic load_en,
     input logic rst,
     input logic clock,
-    output logic [15:0] out
+    output logic [15:0] out,
+    output logic ovl_det
 );
 
-logic [15:0] state;
+logic [16:0] state;
 
 initial begin
-    state = 16'h0;
+    state = 17'h0;
+    ovl_det = 1'b0;
 end
 
 always_ff @(posedge clock or posedge rst) begin
-    if (rst)
-        state <= 16'h0;
-    else if (load_en)
+    if (rst) begin
+        state <= 17'h0;
+        ovl_det <= 1'b0;
+    end else if (load_en) begin
         state <= load;
-    else
+        ovl_det <= 1'b0;
+    end else if (state + in > 17'h0FFFF) begin
+        state <= 17'h0FFFF;
+        ovl_det <= 1'b1;
+    end else begin
         state <= state + in;
+        ovl_det <= 1'b0;
+    end
 end
 
-assign out = state;
+assign out = state[15:0];
 
 endmodule
